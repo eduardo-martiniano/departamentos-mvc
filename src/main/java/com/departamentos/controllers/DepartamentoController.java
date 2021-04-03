dea511c9e03a5ccda09775b24c2d7e82a8c2a5f7
Eduardo Martiniano <eeduardo.martiniano@gmail.com>
2021-04-02 23:59:26 -0300

    Edição e exclusão de departamento

--- src/main/java/com/departamentos/controllers/DepartamentoController.java
+++ src/main/java/com/departamentos/controllers/DepartamentoController.java
@@ -5,12 +5,10 @@ import com.departamentos.dtos.MensagemDto;
 import com.departamentos.services.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+@RestController
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
@@ -28,14 +26,29 @@ public class DepartamentoController {
     @PostMapping("/cadastrar")
     public ModelAndView cadastrar(Departamento departamento) {
         ModelAndView mv = new ModelAndView("/departamento/cadastro");
-        mv.addObject("departamento", departamentoService.salvar(departamento));
+        departamentoService.salvar(departamento);
+        mv.addObject("departamento", new Departamento());
         mv.addObject("mensagem", new MensagemDto(true, "Cadastrado com sucesso!"));
         return mv;
     }
 
     @GetMapping("/listar")
     public ModelAndView listar() {
-        ModelAndView mv = new ModelAndView("/templates/departamento/lista");
+        ModelAndView mv = new ModelAndView("/departamento/lista");
+        mv.addObject("departamentos", departamentoService.lista());
+        return mv;
+    }
+
+    @RequestMapping(value = "/excluir/{id}", method = RequestMethod.DELETE)
+    public void excluir(@PathVariable("id") Long id) {
+        departamentoService.excluir(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ModelAndView editar(@PathVariable("id") Long id, Departamento departamento){
+        departamentoService.editar(departamento);
+        ModelAndView mv = new ModelAndView("/departamento/lista");
+        mv.addObject("departamentos", departamentoService.lista());
         return mv;
     }
 }
--- src/main/java/com/departamentos/services/DepartamentoService.java
+++ src/main/java/com/departamentos/services/DepartamentoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -20,5 +21,20 @@ public class DepartamentoService {
 
     public List<Departamento> lista() {
         return departamentoRepository.findAll();
+    }
+
+    public void excluir(Long id) {
+        Departamento departamento = departamentoRepository.findById(id).orElse(null);
+        if (departamento != null) {
+            departamentoRepository.delete(departamento);
+        }
+    }
+
+    public void editar(Departamento departamento) {
+        departamentoRepository.findById(departamento.getId()).map(d -> {
+            d.setNome(departamento.getNome());
+            departamentoRepository.save(d);
+            return true;
+        });
     }
 }
